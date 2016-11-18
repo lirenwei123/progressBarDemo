8a1d64c1562c8349f1c2bcf59057d21e2bd70bce
Capital <303624150@qq.com>
2016-11-18 20:28:09 +0800

    Initial Commit

---
+++ progressBarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  progressBarDemo
+//
+//  Created by rwli on 16/11/18.
+//  Copyright © 2016年 companyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ progressBarDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  progressBarDemo
+//
+//  Created by rwli on 16/11/18.
+//  Copyright © 2016年 companyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
